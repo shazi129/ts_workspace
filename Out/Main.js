533b18818d218ec6907bf98c2028dabfdcd78441
zhangwen <apply_count@sina.com>
2025-03-16 23:08:45 +0800

    add js to git ignore

--- Out/Main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ClassDecorator_1 = require("./Test/Decorator/ClassDecorator");
-(0, ClassDecorator_1.TestDecorator)();
-//# sourceMappingURL=Main.js.map
